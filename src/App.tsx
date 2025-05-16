8addd8ab0d0f004405b70ff773964287a80136bc
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2025-05-16 16:04:25 +0600

    add shadcnui support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import { Input } from '@/components/ui/input.tsx';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 const App = () => (
-    <div className="flex h-full">
-      <div className="mx-auto">test</div>
+    <div className="flex h-full font-kanit">
+      <div className="mx-auto flex flex-col items-center">
+        <div className="pt-[2.5rem]">
+          <h1 className="text-custom-black font-medium text-[1.625rem] uppercase mb-lg">todo list</h1>
+        </div>
+        <div className="flex">
+          <Input/>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+          <button>switch</button>
+        </div>
+      </div>
+
     </div>
 );
 
